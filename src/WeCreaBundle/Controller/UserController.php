2db64f92841a506c4325b7efd0b1cb78e3f0b011
Anthooo <anthony.menet@gmail.com>
2018-01-24 12:55:37 +0100

    order actu index controller

--- src/WeCreaBundle/Controller/UserController.php
+++ src/WeCreaBundle/Controller/UserController.php
@@ -37,7 +37,7 @@ class UserController extends Controller
         shuffle($carrousels);
         $bCount = $container->get('app.basket')->countBasket($session);
         $fCount = $container->get('favs')->countFavs($session);
-        $actus = $em->getRepository('WeCreaBundle:Actu')->findAll();
+        $actus = $em->getRepository('WeCreaBundle:Actu')->findBy([], ["date" => "DESC"]);
 
         return $this->render('WeCreaBundle:User:index.html.twig', array(
             'carrousels' => $carrousels,
